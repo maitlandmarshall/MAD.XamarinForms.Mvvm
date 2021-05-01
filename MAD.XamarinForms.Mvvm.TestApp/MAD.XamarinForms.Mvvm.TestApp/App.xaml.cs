54151e256c23e4beb7ec801773dfd05f8379b90b
maitlandmarshall <maitlandmarshallapps@gmail.com>
2021-05-01 17:11:32 +1000

    Add Xamarin Forms test app project

---
+++ MAD.XamarinForms.Mvvm.TestApp/MAD.XamarinForms.Mvvm.TestApp/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MAD.XamarinForms.Mvvm.TestApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            Mvvm.Init<Startup>();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MAD.XamarinForms.Mvvm.TestApp/MAD.XamarinForms.Mvvm.TestApp/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MAD.XamarinForms.Mvvm.TestApp
+{
+    internal class Startup
+    {
+        public void ConfigureServices(IServiceCollection serviceDescriptors)
+        {
+
+        }
+
+        public void Configure(MvvmApp mvvmApp)
+        {
+
+        }
+    }
+}
