32710da28d87c4850cc3657864022466a9da1325
maitlandmarshall <maitlandmarshallapps@gmail.com>
2021-05-24 21:39:37 +1000

    Add AddViewModels service collection helper

---
+++ MAD.XamarinForms.Mvvm/ViewModelServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace MAD.XamarinForms.Mvvm
+{
+    public static class ViewModelServiceCollectionExtensions
+    {
+        public static IServiceCollection AddViewModels(this IServiceCollection serviceDescriptors, Assembly assembly = null)
+        {
+            assembly = assembly ?? Assembly.GetCallingAssembly();
+
+            var viewModelTypes = assembly.GetTypes()
+               .Where(y => typeof(ViewModel).IsAssignableFrom(y))
+               .Where(y => y.IsAbstract == false);
+
+            foreach (var vmt in viewModelTypes)
+            {
+                serviceDescriptors.AddScoped(vmt);
+            }
+
+            return serviceDescriptors;
+        }
+    }
+}
